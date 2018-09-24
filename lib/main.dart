e2705aa06466a8f00a5feb9c7fe13470fdc6df44
Jorge Coca <jorge.coca@bmwna.com>
2018-09-24 07:18:45 -0500

    Completed quiz #15 - Show list of categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unit_converter/category.dart';
-
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
+import 'package:unit_converter/category_route.dart';
 
 void main() => runApp(UnitConverterApp());
 
@@ -11,16 +7,9 @@ class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Unit Converter',
-        home: Scaffold(
-          backgroundColor: Colors.green[100],
-          body: Center(
-            child: Category(
-                name: _categoryName,
-                color: _categoryColor,
-                iconLocation: _categoryIcon),
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
+    );
   }
 }
